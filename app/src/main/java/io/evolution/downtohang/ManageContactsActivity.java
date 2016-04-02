718eace9300210f64abcd4ba70c4796e9fc7663f
Patrick Mayo <pmayo21@gmail.com>
2016-04-02 16:04:44 -0400

    Filled out some of the ManageContactsActivity

--- app/src/main/java/io/evolution/downtohang/ManageContactsActivity.java
+++ app/src/main/java/io/evolution/downtohang/ManageContactsActivity.java
@@ -1,11 +1,40 @@
 package io.evolution.downtohang;
 
 import android.app.Activity;
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 /**
  * Created by Patrick on 4/2/2016.
  */
 public class ManageContactsActivity extends Activity {
 
+    private TextView manageContactsSearchUserLabel;
+    private EditText manageContactsSearchUserEditText;
+    private ImageView manageContactsSearchIconImageView;
+    private ListView manageContactsSearchedUsersListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.manage_contacts);
+
+        //Get references to widgets
+        manageContactsSearchUserLabel = (TextView) findViewById(R.id.manageContactsSearchUserLabel);
+        manageContactsSearchUserEditText = (EditText) findViewById(R.id.manageContactsSearchUserEditText);
+        manageContactsSearchIconImageView = (ImageView) findViewById(R.id.manageContactsSearchIconImageView);
+        manageContactsSearchedUsersListView = (ListView) findViewById(R.id.manageContactsSearchedUsersListView);
+
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        //do stuff
+    }
 }
