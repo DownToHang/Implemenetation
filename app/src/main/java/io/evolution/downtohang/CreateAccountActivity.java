b3f9d80432358738d10c7cc5925de551cdae7e0b
ezekie97 <Wezekiel3college@gmx.com>
2016-04-02 18:15:23 -0400

    Cleaned up CreateAccountLayout

--- app/src/main/java/io/evolution/downtohang/CreateAccountActivity.java
+++ app/src/main/java/io/evolution/downtohang/CreateAccountActivity.java
@@ -1,6 +1,8 @@
 package io.evolution.downtohang;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,8 +10,13 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CreateAccountActivity extends Activity
     implements View.OnClickListener {
+
+    private static final int PICK_PROFILE_ICON_IMAGE = 100;
 
     private TextView usernameLabel;
     private EditText editUsername;
@@ -41,19 +48,21 @@ public class CreateAccountActivity extends Activity
     public void onClick(View v) {
         switch(v.getId()) {
             case R.id.createAccountSelectImageButton:
-                setErrorMessage(null);
+                goToImageSelect();
                 break;
             case R.id.createAccountCreateAccountButton:
-                setErrorMessage(getResources().getString(R.string.error_message_db_failed));
+                if(createAccount(editUsername.getText().toString())) {
+                    setErrorMessage("Successful!");
+                };
         }
     }
 
+    /**
+     * Set the text of the error label.
+     * @param message the error message.
+     */
     private void setErrorMessage(String message) {
-        StringBuilder sb = new StringBuilder();
-        if(message != null) {
-            sb.append("Error! ").append(message);
-        }
-        errorLabel.setText(sb.toString());
+        errorLabel.setText(message != null ? message:"");
     }
 
     /**
@@ -62,14 +71,59 @@ public class CreateAccountActivity extends Activity
      * @return true if the creation was successful.
      */
     public boolean createAccount(String username) {
+        return validateUsername(username);
+        /**
         if(validateUsername(username)) {
             // attempt to connect to database.
+            //if(!db.connected()) return false;
         }
         return true;
+         */
     }
 
+    /**
+     * Check if a username is valid. A username is valid if it contains...
+     * @param username the username
+     * @return true if the username is valid.
+     */
     public boolean validateUsername(String username) {
-        String trimmedUsername = username.trim();
-        return trimmedUsername.length() > 0;
+        if(username.length() <= 0) {
+            setErrorMessage("Enter a username!");
+            return false;
+        }
+        String regExp = "^[A-Z,a-z][ A-Z,a-z,0-9,_]*$";
+        Pattern pattern = Pattern.compile(regExp);
+        Matcher matcher = pattern.matcher(username);
+        if(matcher.find()) {
+            return true;
+        }
+        else {
+            setErrorMessage("Invalid username!");
+            return false;
+        }
+    }
+
+    /**
+     * Go to gallery via an intent.
+     */
+    public void goToImageSelect() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"),
+                PICK_PROFILE_ICON_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch(requestCode) {
+                case PICK_PROFILE_ICON_IMAGE:
+                    Uri newProfileIconImage = data.getData();
+                    profileIcon.setImageURI(newProfileIconImage);
+                    break;
+            }
+        }
     }
 }
