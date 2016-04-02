4ec9237a83d0c1d0c7281e3078199ce4e2df187d
Patrick Mayo <pmayo21@gmail.com>
2016-04-02 12:06:19 -0400

    Manage Contacts Activity

---
+++ app/src/main/java/io/evolution/downtohang/ManageContactsActivity.java
@@ -0,0 +1,11 @@
+package io.evolution.downtohang;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Patrick on 4/2/2016.
+ */
+public class ManageContactsActivity extends Activity {
+
+}
