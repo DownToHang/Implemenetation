3628be80e3c739913c0368826fa8b08e6ac9bcec
Joshua Yoon <yoonj21@students.rowan.edu>
2016-04-09 15:32:17 -0400

    Added button function with test code in it for now that displays a toast of people who are selected wihtin the list.

--- app/src/main/java/io/evolution/downtohang/CreateHangoutLayout.java
+++ app/src/main/java/io/evolution/downtohang/CreateHangoutLayout.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,9 +16,9 @@ import java.util.List;
 public class CreateHangoutLayout extends AppCompatActivity{
 
     List<User> users = new ArrayList<User>();
-//    private ListView listView;
     private ListView lv;
     private Context context;
+    private Button hangoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +27,38 @@ public class CreateHangoutLayout extends AppCompatActivity{
 
         lv = (ListView) findViewById(R.id.createHangoutListView);
         context = this;
+        hangoutButton = (Button) findViewById(R.id.hangoutButton);
 
+        //populates list of users
         populateUsers();
+        //populates the listView with items
         populateListView();
+        //sets the onClickListerner for the "Lets Hang!" button
+        setOnClickListener();
+    }
+
+    private void setOnClickListener() {
+        hangoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ArrayList<User> selectedUsers = new ArrayList<User>();
+                for(User u: users){
+                    boolean selected = u.isSelected();
+                    if(selected){
+                        selectedUsers.add(u);
+                    }//end if
+                }//end for
+
+                //this is to test if selected users are correctly selected
+                //this code will be replaced by a new intent passing in a list of user id's
+                String toastMsg = "You have Selected: \n";
+                for (User x: selectedUsers){
+                    toastMsg = toastMsg + x.getUsername()+"\n";
+                }
+
+                Toast.makeText(context, toastMsg, Toast.LENGTH_SHORT).show();
+            }//end onClick
+        });
     }
 
     private void populateUsers() {
@@ -46,8 +77,6 @@ public class CreateHangoutLayout extends AppCompatActivity{
         ArrayAdapter<User> adapter = new MyArrayAdapter();
 
         //Configure the ListView
-//        listView = (ListView) findViewById(R.id.createHangoutListView);
-//        assert listView != null;
         lv.setAdapter(adapter);
     }
 
