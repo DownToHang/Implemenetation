cdc4d22c41b2c32917c92205f289c954bca5e9bd
ezekie97 <Wezekiel3college@gmx.com>
2016-04-09 09:40:29 -0400

    Main Activity status button has some functionality.

--- app/src/main/java/io/evolution/downtohang/MainActivity.java
+++ app/src/main/java/io/evolution/downtohang/MainActivity.java
@@ -1,52 +1,44 @@
 package io.evolution.downtohang;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements OnClickListener {
+
+    private int status = 0;
+    private ImageButton changeStatusImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setContentView(R.layout.main_layout);
+        changeStatusImageButton = (ImageButton) findViewById(R.id.changeStatusImageButton);
+        changeStatusImageButton.setOnClickListener(this);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.changeStatusImageButton:
+                changeStatus();
+        }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private void changeStatus() {
+        if(status == 1) {
+            changeStatusImageButton.setImageResource(R.mipmap.green_circle_icone_4156_128);
+            status = 0;
+        }
+        else {
+            changeStatusImageButton.setImageResource(R.mipmap.red_circle_icone_5751_128);
+            status = 1;
         }
 
-        return super.onOptionsItemSelected(item);
+
     }
 }
